4392a077297955b5c5381791020bf64647479e7c
Aasrith <aasrithvarma2002@gmil.com>
2024-07-07 12:11:44 +0530

    lm

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 UN=$(id -u)
-
+TS=$(date +%F-%H-%M-%S)
+SN=$( echo $0 | cut -d "." -f1)
+LF= /tmp/$SN-$TS.log
 VAL(){
     echo "exit status:$1"
     echo "$2"
 }
 
-if [ $UN -ge 0 ]
+if [ $UN -ne 0 ]
 then
     echo "fuck off"
     exit 1
@@ -15,8 +17,8 @@ else
     echo "you are super usr"
 fi    
 
-dnf install mysql -y
+dnf install mysql -y &>> $LF
 VAL $? "hi"
 
-dnf install git -y
+dnf install git -y &>> $LF
 VAL $? "git"
