295bd298d42edccaffbf2535186acb8426916465
Aasrith <aasrithvarma2002@gmil.com>
2024-07-09 14:57:11 +0530

    lm

--- current.sh
+++ current.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-CU="Devops is very useful"
-
-echo "Before calling script : $CU"
-echo "process id of current script : $$"
-
-./loops.sh
-
-echo "Before calling script : $CU"
+SD=$1
+echo "path:$SD"
